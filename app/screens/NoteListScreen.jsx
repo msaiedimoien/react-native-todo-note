8e43200dfee116ac9e89a914d4c057642cb555b7
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-11-14 23:04:20 +0330

    - Definition of screens

---
+++ app/screens/NoteListScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const NoteListScreen = () => {
+  return (
+    <View>
+      <Text>NoteList Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default NoteListScreen;
---
+++ app/screens/ToDoListScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ToDoListScreen = () => {
+  return (
+    <View>
+      <Text>toDoList Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ToDoListScreen;
